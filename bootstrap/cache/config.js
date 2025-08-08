27c1defd2d8763319ee739632f2ad88b669f0071
Shamim Haque <shamim.haque.dev@gmail.com>
2025-08-08 10:27:24 +0600

    add example env

--- bootstrap/cache/config.js
+++ bootstrap/cache/config.js
@@ -1,8 +1,1 @@
-const app = (await import('file:///home/shamim-haque/Desktop/tech_sync/config/app.js')).default;
-const database = (await import('file:///home/shamim-haque/Desktop/tech_sync/config/database.js')).default;
-
-
-export const configs = {
-  app : app,
-  database : database,
-}
+export const configs = {}
